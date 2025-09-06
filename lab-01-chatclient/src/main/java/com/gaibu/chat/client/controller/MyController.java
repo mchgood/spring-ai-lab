ff883316a148ab58cce4bf404a16086e743c2e8d
gaibu <1016771049@qq.com>
2025-09-06 11:38:31 +0800

    chat-client

---
+++ lab-01-chatclient/src/main/java/com/gaibu/chat/client/controller/MyController.java
@@ -0,0 +1,24 @@
+package com.gaibu.chat.client.controller;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.openai.OpenAiChatOptions;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyController {
+
+    private final ChatClient chatClient;
+
+    public MyController(ChatClient.Builder chatClientBuilder) {
+        this.chatClient = chatClientBuilder.build();
+    }
+
+    @GetMapping("/ai")
+    String generation(String userInput) {
+        return this.chatClient.prompt()
+                .user(userInput)
+                .call()
+                .content();
+    }
+}
