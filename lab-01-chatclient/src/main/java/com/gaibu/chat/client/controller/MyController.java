6ec4e80cfabd5dec3cb5aa3f837ff14cb910d1f6
gaibu <1016771049@qq.com>
2025-09-27 21:51:07 +0800

    配置 chat client  多模型

--- lab-01-chatclient/src/main/java/com/gaibu/chat/client/controller/MyController.java
+++ lab-01-chatclient/src/main/java/com/gaibu/chat/client/controller/MyController.java
@@ -3,8 +3,10 @@ package com.gaibu.chat.client.controller;
 import cn.hutool.core.util.RandomUtil;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 
 import java.util.List;
 
@@ -18,11 +20,42 @@ public class MyController {
     private ChatClient qwenMaxChatClient;
 
     @GetMapping("/ai")
-    String generation(String userInput) {
+    public String generation(String userInput) {
 
         return RandomUtil.randomEle(List.of(qwenPlusChatClient, qwenMaxChatClient)).prompt()
                 .user(userInput)
                 .call()
                 .content();
+    }
+
+    @GetMapping(
+            value = "/ai/stream"
+    )
+    public Flux<String> generationStream(String userInput) {
+
+        return RandomUtil.randomEle(List.of(qwenPlusChatClient, qwenMaxChatClient)).prompt()
+                .user(userInput)
+                .stream()
+                .content();
+    }
+
+    @GetMapping("/ai/returnEntity")
+    public ActorFilms returnEntity() {
+        return qwenMaxChatClient.prompt()
+                .user("Generate the filmography for a random actor.")
+                .call()
+                .entity(ActorFilms.class);
+    }
+
+    @GetMapping("/ai/returnEntityList")
+    public List<ActorFilms> returnEntityList() {
+        return qwenMaxChatClient.prompt()
+                .user("Generate the filmography of 5 movies for Tom Hanks and Bill Murray.")
+                .call()
+                .entity(new ParameterizedTypeReference<List<ActorFilms>>() {
+                });
+    }
+
+    public record ActorFilms(String actor, List<String> movies) {
     }
 }
